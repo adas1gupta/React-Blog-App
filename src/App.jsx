137ddecfb895a37534bab7dbada16c96a4e99324
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-01-22 21:23:27 -0500

    working on this at home

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { useState } from "react";
 import useBlogs from "./hooks/useBlogs";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 function App () {
     const [page, setPage] = useState(1)
     const [loading, setLoading] = useState(true)
     const postsPerPage = 10
     const { blogs, setBlogs } = useBlogs(page, postsPerPage)
+    const queryClient = new QueryClient()
+    const { ?, ? } = useQuery({
+        queryKey: ['past-orders', page],
+        queryFn: () => setBlogs(page, postsPerPage),
+        staleTime: 1000000000
+    })
 
     return (
+        <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
         <div>
             {(loading) && (
                 <div>
@@ -17,14 +27,13 @@ function App () {
                 </div>
             )}
         </div>
+        </QueryClientProvider>
+        </React.StrictMode>
     )
-
 }
 
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 )
